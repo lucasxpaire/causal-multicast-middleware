c806ae1426edded7873df93be4227800f1c5e4e7
lucas <lxpaire@inf.ufsm.br>
2026-06-14 19:32:34 -0300

    Adiciona interface envio de mensagens.

---
+++ src/main/java/CausalMulticast/ICausalMulticast.java
@@ -0,0 +1,8 @@
+package CausalMulticast;
+
+/**
+ * Interface que deve ser implementada por todo usuário do pacote CausalMulticast.
+ */
+public interface ICausalMulticast {
+    public void deliver(String msg);
+}
