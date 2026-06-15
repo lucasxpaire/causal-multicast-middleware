c535d61208884b9a6401ad39c93506959bbbb519
lucas <lxpaire@inf.ufsm.br>
2026-06-14 22:11:00 -0300

    Remove comentário

--- src/main/java/CausalMulticast/ICausalMulticast.java
+++ src/main/java/CausalMulticast/ICausalMulticast.java
@@ -1,8 +1,5 @@
 package CausalMulticast;
 
-/**
- * Interface que deve ser implementada por todo usuário do pacote CausalMulticast.
- */
 public interface ICausalMulticast {
     public void deliver(String msg);
 }
